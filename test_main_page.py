b71049223d9262e30296244fa02093b4b314e900
lopez <lopez-x@yandex.ru>
2020-06-03 18:03:44 +0300

    'asserts of reg and login form added

--- test_main_page.py
+++ test_main_page.py
@@ -6,3 +6,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпл€р драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполн€ем метод страницы - переходим на страницу логина
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
